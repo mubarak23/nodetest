1a3a2337c8fe583ae376658c8467e71060852aed
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-12-15 20:38:20 +0100

    setup the view pages

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var MongoStore = require('connect-mongo')(session);
 var passport = require('passport');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
 
 var app = express();
 
-mongoose.connect('mongodb://root:root123@ds251240.mlab.com:51240/nodehome', function(err) {
+mongoose.connect('mongodb://root:root123@ds251240.mlab.com:51240/nodehome',
+ { useNewUrlParser: true },  function(err) {
 		if(err){
 			console.log(err);
 		}else{
@@ -18,16 +21,38 @@ mongoose.connect('mongodb://root:root123@ds251240.mlab.com:51240/nodehome', func
 		}
 });
 
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({
-	resave: true;
-	saveUnitialized: true;
-	secret: "Hello";
-	store: new MongoStore({ url: 'mongodb://root:root123@ds251240.mlab.com:51240/nodehome', autoReconnect: true;});
+	resave: true,
+	saveUnitialized: true,
+	secret: "Hello",
+	store: new MongoStore({ url: 'mongodb://root:root123@ds251240.mlab.com:51240/nodehome', autoReconnect: true })
 }));
+
+app.get('/', function(req, res, next){
+	res.render('home');
+})
+
+app.get('/login', function(req, res, next){
+	if(req.user) return res.redirect('/');
+	res.render('login');
+});
+
+app.post('/login', passport.authenticate('local-login', {
+	successRedirect: '/profile',
+	failureRedirect: '/login',
+}));
+
+app.get('/profile', function(req, res, next){
+		res.render('profile');
+});
+
+
 
 app.listen(500, function(err){
 		if(err){
